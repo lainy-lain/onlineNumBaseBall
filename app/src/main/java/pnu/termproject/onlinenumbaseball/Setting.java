ff76040a8fd48da752eef01301b1eb868dca6b44
fabbitox <hyunjin_11@naver.com>
2021-04-11 17:17:40 +0900

    background color selection part is added

--- app/src/main/java/pnu/termproject/onlinenumbaseball/Setting.java
+++ app/src/main/java/pnu/termproject/onlinenumbaseball/Setting.java
@@ -28,26 +28,34 @@ import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
 
 public class Setting extends AppCompatActivity {
+    private View currentView;
+    private ScrollView[] willBeAdd = new ScrollView[4];
+    private boolean[] isLoaded = {false, false, false, false};
+    private LinearLayout colorKind;
+    private Switch bgOrText;
+    private LinearLayout[] simpleColors = new LinearLayout[3];
+    Button set_bgbtn;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        bgOrText = findViewById(R.id.bg_text);
         Button[] buttons = {
                 findViewById(R.id.play_setbtn), findViewById(R.id.single_setbtn), findViewById(R.id.multi_setbtn)
-                , findViewById(R.id.rank_setbtn), findViewById(R.id.set_setbtn)
+                , findViewById(R.id.rank_setbtn), findViewById(R.id.set_setbtn), findViewById(R.id.set_bgbtn)
         };
-        LinearLayout colorKind = findViewById(R.id.colorKind);
+        set_bgbtn = findViewById(R.id.set_bgbtn);
+        colorKind = findViewById(R.id.colorKind);
         Button[] colorKindButtons = {
                 findViewById(R.id.red), findViewById(R.id.green), findViewById(R.id.blue)
                 , findViewById(R.id.yellow), findViewById(R.id.cyan), findViewById(R.id.magenta)
                 , findViewById(R.id.other)
         };
-        LinearLayout[] simpleColors = {
-                findViewById(R.id.reds), findViewById(R.id.greens), findViewById(R.id.blues)
-        };
-
+        simpleColors[0] = findViewById(R.id.reds);
+        simpleColors[1] = findViewById(R.id.greens);
+        simpleColors[2] = findViewById(R.id.blues);
         Button[] colorButtons = new Button[16];
         colorButtons[0] = findViewById(R.id.black);
         for (int i = 0; i < 5; i++) {
@@ -55,16 +63,15 @@ public class Setting extends AppCompatActivity {
             colorButtons[6 + i] = (Button)simpleColors[1].getChildAt(i);
             colorButtons[11 + i] = (Button)simpleColors[2].getChildAt(i);
         }
+        willBeAdd[0] = findViewById(R.id.forY);
+        willBeAdd[1] = findViewById(R.id.forC);
+        willBeAdd[2] = findViewById(R.id.forM);
+        willBeAdd[3] = findViewById(R.id.forO);
 
-        Switch bgOrText = findViewById(R.id.bg_text);
-        View[] currentView = new View[1];
-        ScrollView[] willBeAdd = {findViewById(R.id.forY), findViewById(R.id.forC), findViewById(R.id.forM), findViewById(R.id.forO)};
-        boolean[] isLoaded = {false, false, false, false};
-
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             buttons[i].setOnClickListener(v -> {
                 colorKind.setY(v.getY());
-                setCurrent(currentView, v);
+                setCurrent(v);
                 setInvisibleSimples(simpleColors);
                 setInvisibleScrollViews(willBeAdd, isLoaded);
                 if (colorKind.getVisibility() == INVISIBLE) {
@@ -75,171 +82,24 @@ public class Setting extends AppCompatActivity {
             });
         }
 
-        colorKindButtons[0].setOnClickListener(v -> {
-            simpleColors[0].setY(currentView[0].getY() + colorKind.getHeight());
-            if (simpleColors[0].getVisibility() == INVISIBLE) {
-                setInvisibleSimples(simpleColors);
-                setInvisibleScrollViews(willBeAdd, isLoaded);
-                simpleColors[0].setVisibility(VISIBLE);
-            } else {
-                simpleColors[0].setVisibility(INVISIBLE);
-            }
-        });
-        colorKindButtons[1].setOnClickListener(v -> {
-            simpleColors[1].setY(currentView[0].getY() + colorKind.getHeight());
-            if (simpleColors[1].getVisibility() == INVISIBLE) {
-                setInvisibleSimples(simpleColors);
-                setInvisibleScrollViews(willBeAdd, isLoaded);
-                simpleColors[1].setVisibility(VISIBLE);
-            } else {
-                simpleColors[1].setVisibility(INVISIBLE);
-            }
-        });
-        colorKindButtons[2].setOnClickListener(v -> {
-            simpleColors[2].setY(currentView[0].getY() + colorKind.getHeight());
-            if (simpleColors[2].getVisibility() == INVISIBLE) {
-                setInvisibleSimples(simpleColors);
-                setInvisibleScrollViews(willBeAdd, isLoaded);
-                simpleColors[2].setVisibility(VISIBLE);
-            } else {
-                simpleColors[2].setVisibility(INVISIBLE);
-            }
-        });
-
-        for (int i = 0; i < 16; i++) {
-            colorButtons[i].setOnClickListener(v -> {
-                if (bgOrText.isChecked()) {
-                    ((Button)currentView[0]).setTextColor(v.getBackgroundTintList());
-                } else {
-                    currentView[0].setBackgroundTintList(v.getBackgroundTintList());
-                }
-            });
+        for (int i = 0; i < 3; i++) {
+            setOnClickSimple(colorKindButtons, i);
         }
 
-        colorKindButtons[3].setOnClickListener(new View.OnClickListener() {
-            boolean first = true;
-            TableLayout yellows;
-            @Override
-            public void onClick(View v) {
-                if (first) {
-                    willBeAdd[0].setY(currentView[0].getY() + colorKind.getHeight());
-                    LayoutInflater inflater = getLayoutInflater();
-                    inflater.inflate(R.layout.colors_y, willBeAdd[0], true);
-                    yellows = findViewById(R.id.yellows);
-                    isLoaded[0] = true;
-                    first = false;
-                    willBeAdd[0].setVisibility(INVISIBLE);
-                    Button[] btn_yellows = new Button[25];
-                    for (int i = 0; i < 5; i++) {
-                        for (int j = 0; j < 5; j++) {
-                            btn_yellows[i*5 + j] = (Button)((TableRow)yellows.getChildAt(i)).getChildAt(j);
-                        }
-                    }
-                    for (int i = 0; i < 25; i++) {
-                        btn_yellows[i].setOnClickListener(v1 -> {
-                            if (bgOrText.isChecked()) {
-                                ((Button)currentView[0]).setTextColor(v1.getBackgroundTintList());
-                            } else {
-                                currentView[0].setBackgroundTintList(v1.getBackgroundTintList());
-                            }
-                        });
-                    }
-                }
-                willBeAdd[0].setY(currentView[0].getY() + colorKind.getHeight());
-                if (willBeAdd[0].getVisibility() == INVISIBLE) {
-                    setInvisibleSimples(simpleColors);
-                    setInvisibleScrollViews(willBeAdd, isLoaded);
-                    willBeAdd[0].setVisibility(VISIBLE);
-                } else {
-                    willBeAdd[0].setVisibility(INVISIBLE);
-                }
-            }
-        });
-        colorKindButtons[4].setOnClickListener(new View.OnClickListener() {
-            boolean first = true;
-            TableLayout cyans;
-            @Override
-            public void onClick(View v) {
-                if (first) {
-                    willBeAdd[1].setY(currentView[0].getY() + colorKind.getHeight());
-                    LayoutInflater inflater = getLayoutInflater();
-                    inflater.inflate(R.layout.colors_c, willBeAdd[1], true);
-                    cyans = findViewById(R.id.cyans);
-                    isLoaded[1] = true;
-                    first = false;
-                    willBeAdd[1].setVisibility(INVISIBLE);
-                    Button[] btn_cyans = new Button[25];
-                    for (int i = 0; i < 5; i++) {
-                        for (int j = 0; j < 5; j++) {
-                            btn_cyans[i*5 + j] = (Button)((TableRow)cyans.getChildAt(i)).getChildAt(j);
-                        }
-                    }
-                    for (int i = 0; i < 25; i++) {
-                        btn_cyans[i].setOnClickListener(v12 -> {
-                            if (bgOrText.isChecked()) {
-                                ((Button)currentView[0]).setTextColor(v12.getBackgroundTintList());
-                            } else {
-                                currentView[0].setBackgroundTintList(v12.getBackgroundTintList());
-                            }
-                        });
-                    }
-                }
-                willBeAdd[1].setY(currentView[0].getY() + colorKind.getHeight());
-                if (willBeAdd[1].getVisibility() == INVISIBLE) {
-                    setInvisibleSimples(simpleColors);
-                    setInvisibleScrollViews(willBeAdd, isLoaded);
-                    willBeAdd[1].setVisibility(VISIBLE);
-                } else {
-                    willBeAdd[1].setVisibility(INVISIBLE);
-                }
-            }
-        });
-        colorKindButtons[5].setOnClickListener(new View.OnClickListener() {
-            boolean first = true;
-            TableLayout magentas;
-            @Override
-            public void onClick(View v) {
-                if (first) {
-                    willBeAdd[2].setY(currentView[0].getY() + colorKind.getHeight());
-                    LayoutInflater inflater = getLayoutInflater();
-                    inflater.inflate(R.layout.colors_m, willBeAdd[2], true);
-                    magentas = findViewById(R.id.magentas);
-                    isLoaded[2] = true;
-                    first = false;
-                    willBeAdd[2].setVisibility(INVISIBLE);
-                    Button[] btn_magentas = new Button[25];
-                    for (int i = 0; i < 5; i++) {
-                        for (int j = 0; j < 5; j++) {
-                            btn_magentas[i*5 + j] = (Button)((TableRow)magentas.getChildAt(i)).getChildAt(j);
-                        }
-                    }
-                    for (int i = 0; i < 25; i++) {
-                        btn_magentas[i].setOnClickListener(v13 -> {
-                            if (bgOrText.isChecked()) {
-                                ((Button)currentView[0]).setTextColor(v13.getBackgroundTintList());
-                            } else {
-                                currentView[0].setBackgroundTintList(v13.getBackgroundTintList());
-                            }
-                        });
-                    }
-                }
-                willBeAdd[2].setY(currentView[0].getY() + colorKind.getHeight());
-                if (willBeAdd[2].getVisibility() == INVISIBLE) {
-                    setInvisibleSimples(simpleColors);
-                    setInvisibleScrollViews(willBeAdd, isLoaded);
-                    willBeAdd[2].setVisibility(VISIBLE);
-                } else {
-                    willBeAdd[2].setVisibility(INVISIBLE);
-                }
-            }
-        });
+        for (int i = 0; i < 16; i++) {
+            setOnClickColorBtn(colorButtons[i]);
+        }
+
+        setOnClickComplex(colorKindButtons[3], 0, R.layout.colors_y, R.id.yellows);
+        setOnClickComplex(colorKindButtons[4], 1, R.layout.colors_c, R.id.cyans);
+        setOnClickComplex(colorKindButtons[5], 2, R.layout.colors_m, R.id.magentas);
         colorKindButtons[6].setOnClickListener(new View.OnClickListener() {
             boolean first = true;
             TableLayout others;
             @Override
             public void onClick(View v) {
                 if (first) {
-                    willBeAdd[3].setY(currentView[0].getY() + colorKind.getHeight());
+                    willBeAdd[3].setY(currentView.getY() + colorKind.getHeight());
                     LayoutInflater inflater = getLayoutInflater();
                     inflater.inflate(R.layout.colors_others, willBeAdd[3], true);
                     others = findViewById(R.id.others);
@@ -256,16 +116,10 @@ public class Setting extends AppCompatActivity {
                         btn_others[120 + i] = (Button)((TableRow)others.getChildAt(12)).getChildAt(i);
                     }
                     for (int i = 0; i < 125; i++) {
-                        btn_others[i].setOnClickListener(v14 -> {
-                            if (bgOrText.isChecked()) {
-                                ((Button)currentView[0]).setTextColor(v14.getBackgroundTintList());
-                            } else {
-                                currentView[0].setBackgroundTintList(v14.getBackgroundTintList());
-                            }
-                        });
+                        setOnClickColorBtn(btn_others[i]);
                     }
                 }
-                willBeAdd[3].setY(currentView[0].getY() + colorKind.getHeight());
+                willBeAdd[3].setY(currentView.getY() + colorKind.getHeight());
                 if (willBeAdd[3].getVisibility() == INVISIBLE) {
                     setInvisibleSimples(simpleColors);
                     setInvisibleScrollViews(willBeAdd, isLoaded);
@@ -277,8 +131,8 @@ public class Setting extends AppCompatActivity {
         });
     }
 
-    private void setCurrent(View[] current, View view) {
-        current[0] = view;
+    private void setCurrent(View view) {
+        currentView = view;
     }
 
     private void setInvisibleSimples(LinearLayout[] simples) {
@@ -295,5 +149,73 @@ public class Setting extends AppCompatActivity {
                 willBeAdd[i].setVisibility(INVISIBLE);
             }
         }
+    }
+
+    private void setOnClickSimple(Button[] buttons, int index) {
+        buttons[index].setOnClickListener(v -> {
+            simpleColors[index].setY(currentView.getY() + colorKind.getHeight());
+            if (simpleColors[index].getVisibility() == INVISIBLE) {
+                setInvisibleSimples(simpleColors);
+                setInvisibleScrollViews(willBeAdd, isLoaded);
+                simpleColors[index].setVisibility(VISIBLE);
+            } else {
+                simpleColors[index].setVisibility(INVISIBLE);
+            }
+        });
+    }
+
+    private void setOnClickComplex(Button button, int index, int layout, int id) {
+        button.setOnClickListener(new View.OnClickListener() {
+            boolean first = true;
+            TableLayout colors;
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                if (first) {
+                    willBeAdd[index].setY(currentView.getY() + colorKind.getHeight());
+                    LayoutInflater inflater = getLayoutInflater();
+                    inflater.inflate(layout, willBeAdd[index], true);
+                    colors = findViewById(id);
+                    isLoaded[index] = true;
+                    first = false;
+                    willBeAdd[index].setVisibility(INVISIBLE);
+                    Button[] btn_colors = new Button[25];
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j < 5; j++) {
+                            btn_colors[i*5 + j] = (Button)((TableRow)colors.getChildAt(i)).getChildAt(j);
+                        }
+                    }
+                    for (int i = 0; i < 25; i++) {
+                        setOnClickColorBtn(btn_colors[i]);
+                    }
+                }
+                willBeAdd[index].setY(currentView.getY() + colorKind.getHeight());
+                if (willBeAdd[index].getVisibility() == INVISIBLE) {
+                    setInvisibleSimples(simpleColors);
+                    setInvisibleScrollViews(willBeAdd, isLoaded);
+                    willBeAdd[index].setVisibility(VISIBLE);
+                } else {
+                    willBeAdd[index].setVisibility(INVISIBLE);
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setOnClickColorBtn(Button button) {
+        button.setOnClickListener(v -> {
+            if (bgOrText.isChecked()) {
+                ((Button)currentView).setTextColor(v.getBackgroundTintList());
+            } else {
+                currentView.setBackgroundTintList(v.getBackgroundTintList());
+            }
+            if (currentView == set_bgbtn) {
+                if (bgOrText.isChecked()) {
+                    bgOrText.setTextColor(v.getBackgroundTintList());
+                } else {
+                    (currentView.getRootView()).setBackgroundTintList(v.getBackgroundTintList());
+                }
+            }
+        });
     }
 }
