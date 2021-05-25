f240884f015e420022827987dce3d5fd435841fc
Jung-Haewon <speedhaewon@naver.com>
2021-05-25 19:51:20 +0900

    대기방 리스트
    기본적인 기능은 모두 구현했습니다.
    빠른 시작은, 추후에 기능을 추가하겠습니다.
    
    intent() 수정해야 할 부분이 2군데 있습니다.
    모두 만드신 class로 변경해 주세요!

--- app/src/main/java/pnu/termproject/onlinenumbaseball/MultiList.java
+++ app/src/main/java/pnu/termproject/onlinenumbaseball/MultiList.java
@@ -1,13 +1,133 @@
 package pnu.termproject.onlinenumbaseball;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.icu.text.Edits;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class MultiList extends AppCompatActivity {
+
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> arr_roomList = new ArrayList<>();
+    private ListView listView;
+    private Button btn_create, btn_quick;
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference().getRoot().child("room");
+    private String str_name, str_room;
+    private FirebaseUser currentUser;
+    private User user;
+    private boolean isOrder; //대기방의 생성자인지, 참가자인지 구별하는 변수
+
+    Map<String, Object> map = new HashMap<String, Object>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multiplay_list);
+
+        listView = findViewById(R.id.list);
+        btn_create = findViewById(R.id.btn_create);
+        btn_quick = findViewById(R.id.btn_quick);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr_roomList);
+        listView.setAdapter(arrayAdapter);
+
+        //채팅방을 생성하는 코드입니다
+        btn_create.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final EditText et_inDialog = new EditText(MultiList.this);
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MultiList.this);
+                builder.setTitle("대기방 이름 입력");
+                builder.setView(et_inDialog);
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        str_room = et_inDialog.getText().toString();
+                        isOrder = true;
+                        //☆☆☆☆☆☆☆☆첫번째 수정해야할 부분입니다! MainActivity부분을 수정해주세요☆☆☆☆☆☆☆☆
+
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.putExtra("room-name", str_room);
+                        intent.putExtra("isOrder", isOrder);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
+            }
+        });
+
+        //Database가 변경되었을때 호출되는 코드입니다
+        //실시간으로 변경사항을 감지하고 업데이트 합니다
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Set<String> set = new HashSet<String>();
+                Iterator i = snapshot.getChildren().iterator();
+
+                while(i.hasNext()){
+                    set.add(((DataSnapshot) i.next()).getKey());
+                }
+
+                arr_roomList.clear();
+                arr_roomList.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            //취소되었을때 호출되는 코드입니다
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        //대기방리스트중 하나를 클릭했을때의 코드입니다
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            //"방이름"과 "생성자 여부"를 전송합니다
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //☆☆☆☆☆☆☆☆두번째 수정해야 할 부분입니다. MainActivity부분만 수정해 주시면 됩니다☆☆☆☆☆☆☆☆
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                isOrder = false;
+                intent.putExtra("room-name", ((TextView) view).getText().toString());
+                intent.putExtra("isOrder", isOrder);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
